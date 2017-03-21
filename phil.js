1bbb67e9c3fdede60d7882bddb5e39795a09533f
PhilLutkovsky <7plutkovsky@stpatrick.org>
2017-03-21 14:00:03 -0500

    Add files via upload

---
+++ phil.js
@@ -0,0 +1,13 @@
+//declare samecolor() here
+function samecolor(hex){
+    const doc = document;
+    let divs = doc.querySelectorAll("#samecolor div");
+
+    for(var i=0; i<divs.length; i++){
+        div[i].style.backgroundColor = "#000000"
+    }
+}
+//declare diffcolor() here
+function diffcolor(){
+
+}
