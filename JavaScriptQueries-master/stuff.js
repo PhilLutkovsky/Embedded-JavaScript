5c0dd2218d79402f3aea8cbf1751b6ff87dcf894
PhilLutkovsky <7plutkovsky@stpatrick.org>
2017-03-22 14:03:30 -0500

    More done

---
+++ JavaScriptQueries-master/stuff.js
@@ -0,0 +1,19 @@
+//declare samecolor() here
+function samecolor(){
+    const doc = document;
+    let divs = doc.querySelectorAll("#samecolor div");
+
+    for(var i=0; i<divs.length; i++){
+        divs[i].style.backgroundColor = "fa0000"
+        let temp = Math.random();
+        divs[i].style.opacity = temp
+    }
+}
+//declare diffcolor() here
+function diffcolor(){
+  const doc = document;
+  let divs = doc.querySelectorAll("#diffcolor > div");
+  for(var i=0; i<divs.length; i++){
+    if
+  }
+}
