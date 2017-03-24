18d9a8927962ab38a327bd742da3bbcd91b22730
PhilLutkovsky <7plutkovsky@stpatrick.org>
2017-03-24 13:56:55 -0500

    Add files via upload

---
+++ finished.js
@@ -0,0 +1,26 @@
+//declare samecolor() here
+function samecolor(){
+    const doc = document;
+    let divs = doc.querySelectorAll("#samecolor div");
+
+    for(var i=0; i<divs.length; i++){
+        divs[i].style.backgroundColor = "fa0000"
+        let temp = Math.random();
+        divs[i].style.opacity = temp;
+    }
+}
+//declare diffcolor() here
+function diffcolor(){
+  const doc = document;
+  let divs = doc.querySelectorAll("#diffcolor > div");
+
+  for(var i=0; i<divs.length; i++){
+    if(i % 2 == 0){
+     divs[i].style.backgroundColor = "#ff0000";
+ }
+  else{
+    divs[i].style.backgroundColor = "#00ffff";
+  }
+
+}
+}
